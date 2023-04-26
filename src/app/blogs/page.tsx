e36abd5c3cbe4210dd99622cf3b4644ecab1108f
kevin68107 <kevin.aihoon@amalitech.com>
2023-04-26 11:12:16 +0000

    merged develop into blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -12,11 +12,11 @@ type IndexPageRef = React.ForwardedRef<HTMLDivElement>;
 const Page = (ref: IndexPageRef) => (
   <PageTransition ref={ref}>
     <Link href={"./blogs/blog"} ><h1 className={"text-2xl"}>All Blogs Page</h1></Link>
-    <div className="box-border p-4 border-2 rounded-md">
+    <div className="box-border p-4 border-2 rounded-md mt-4">
       <div className="flex space-x-4">
         <div className="">
           <h1 className="text-3xl font-bold text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, rem.</h1>
-          <p className="text-slate-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores quibusdam aspernatur dicta quo 
+          <p className="text-slate-400 pt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores quibusdam aspernatur dicta quo 
             commodi id sint autem dignissimos delectus maiores!</p>
         </div>
 
