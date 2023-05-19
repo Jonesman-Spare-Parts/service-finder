d0a00e48029bf13c3c5087ee6e6eb12207ef6338
samuel <>
2023-05-19 11:09:16 +0000

    integrated cms into blog page

--- src/components/BlogSummary.tsx
+++ src/components/BlogSummary.tsx
@@ -25,7 +25,7 @@ export function BlogSummary({ blog }: BlogSummary) {
           <Image
             src={blog.image}
             alt=""
-            width={250}
+            width={300}
             height={200}
             className={"rounded-lg"}
           />
